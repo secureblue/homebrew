4eebbadac22b29119c5d8989bd5c7805621bdd00
Daniel Hast <hast.daniel@protonmail.com>
2026-01-16 12:14:07 -0500

    fix: avoid naming conflict

--- copr-script.sh
+++ copr-script.sh
@@ -8,5 +8,5 @@ set -euxo pipefail
 
 latest_tag=$(curl -fLsS --retry 3 'https://api.github.com/repos/Homebrew/brew/releases/latest' | jq -cr '.tag_name')
 
-curl -fLsS --retry 3 'https://raw.githubusercontent.com/HastD/homebrew/refs/heads/main/homebrew.spec' \
+curl -fLsS --retry 3 'https://raw.githubusercontent.com/HastD/homebrew/refs/heads/main/homebrew-template.spec' \
     | sed --sandbox -e "s/@@VERSION@@/${latest_tag}/g" > ./homebrew.spec
