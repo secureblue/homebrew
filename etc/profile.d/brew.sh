b7b8c9e6a5569af70b2d6af31b0f6b545ce69324
Daniel Hast <hast.daniel@protonmail.com>
2026-04-07 19:55:40 -0400

    feat: inline brew shellenv for bash and fish
    This should improve efficiency by reducing unnecesary calls to `brew`,
    especially when brew-proxy is in use.

--- etc/profile.d/brew.sh
+++ etc/profile.d/brew.sh
@@ -6,19 +6,17 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-if [ -x /home/linuxbrew/.linuxbrew/bin/brew ]; then
-  case "$(/usr/bin/id -nu)" in
-    root) # Never put brew commands in PATH for root user
-      ;;
-    linuxbrew) # Always put brew commands in PATH for linuxbrew user
-      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-      ;;
-    *) # Put brew commands in PATH for other users only in interactive sessions
-      case "$-" in
-        *i*)
-          eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-          ;;
-      esac
+if [ -x /home/linuxbrew/.linuxbrew/bin/brew ] && [ "$(/usr/bin/id -u)" != 0 ]; then
+  case "$-" in
+    *i*)
+      # Begin output of `brew shellenv bash` (lightly edited for formatting)
+      export HOMEBREW_PREFIX='/home/linuxbrew/.linuxbrew'
+      export HOMEBREW_CELLAR='/home/linuxbrew/.linuxbrew/Cellar'
+      export HOMEBREW_REPOSITORY='/home/linuxbrew/.linuxbrew/Homebrew'
+      export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}";
+      [ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}"
+      export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}"
+      # End output of `brew shellenv bash`
       ;;
   esac
 fi
--- usr/share/fish/vendor_conf.d/brew-fish-completions.fish
+++ usr/share/fish/vendor_conf.d/brew-fish-completions.fish
@@ -2,18 +2,31 @@
 
 # SPDX-FileCopyrightText: Copyright 2025 Universal Blue
 # SPDX-FileCopyrightText: Copyright 2025 The BlueBuild Authors
+# SPDX-FishCopyrightText: Copyright 2026 Daniel Hast
 #
 # SPDX-License-Identifier: Apache-2.0
 
 #shellcheck disable=all
-if status --is-interactive; and test $(/usr/bin/id -u) -ne 0
-    if [ -d /home/linuxbrew/.linuxbrew ]
-        /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
-        if test -d (brew --prefix)/share/fish/completions
-            set -p fish_complete_path (brew --prefix)/share/fish/completions
+if status --is-interactive && not fish_is_root_user
+    if test -d '/home/linuxbrew/.linuxbrew'
+        # Begin output of `brew shellenv fish` (lightly edited for formatting)
+        set --global --export HOMEBREW_PREFIX '/home/linuxbrew/.linuxbrew'
+        set --global --export HOMEBREW_CELLAR '/home/linuxbrew/.linuxbrew/Cellar'
+        set --global --export HOMEBREW_REPOSITORY '/home/linuxbrew/.linuxbrew/Homebrew'
+        fish_add_path --global --move --path '/home/linuxbrew/.linuxbrew/bin' '/home/linuxbrew/.linuxbrew/sbin'
+        if test -n "$MANPATH[1]"
+            set --global --export MANPATH '' $MANPATH
         end
-        if test -d (brew --prefix)/share/fish/vendor_completions.d
-            set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+        if not contains '/home/linuxbrew/.linuxbrew/share/info' $INFOPATH
+            set --global --export INFOPATH '/home/linuxbrew/.linuxbrew/share/info' $INFOPATH
+        end
+        # End output of `brew shellenv fish`
+
+        if test -d '/home/linuxbrew/.linuxbrew/share/fish/completions'
+            set -p fish_complete_path '/home/linuxbrew/.linuxbrew/share/fish/completions'
+        end
+        if test -d '/home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d'
+            set -p fish_complete_path '/home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d'
         end
     end
 end
